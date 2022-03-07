6c391a60ea08cb197ef20e6f319748bbd2e6c87e
Кирилл Попов <kirill.kiria@yandex.ru>
2022-03-07 21:38:43 +0300

    Добавьте файлы проекта.

---
+++ Task18/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Stack<char> a = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    a.Push(')');
+                }
+                if (s[i] == '{')
+                {
+                    a.Push('}');
+                }
+                if (s[i] == '[')
+                {
+                    a.Push(']');
+                }
+                if ((s[i] == ')') && (s[i].Equals(a.Peek()) == true))
+                {
+                    a.Pop();
+                }
+                if ((s[i] == ']') && (s[i].Equals(a.Peek()) == true))
+                {
+                    a.Pop();
+                }
+                if ((s[i] == '}') && (s[i].Equals(a.Peek()) == true))
+                {
+                    a.Pop();
+                }
+            }
+            if (a.Count==0)
+            {
+                Console.WriteLine("Скобки расставлены корректно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены некорректно");
+            }            
+            Console.ReadKey();
+        }
+    }
+}
